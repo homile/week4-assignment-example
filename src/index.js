6c79ef1de90fb0d2542d36beb75f925826649041
homile <ssanho2@naver.com>
2023-01-18 01:25:30 +0900

    rename : sotre -> store 오타 수정

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import store from "./redux/sotre";
+import store from "./redux/store";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
